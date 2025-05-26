dbf9e7861ba9fd1e2c892f9720862fac6410eb2c
Ondřej Čertík <ondrej@certik.us>
2025-05-26 18:18:46 +0200

    X

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ re2c -b tokenizer.re -o tokenizer.c
 clang $CFLAGS -c tokenizer.c -o tokenizer.o
 clang $CFLAGS -c arena.c -o arena.o
 clang $CFLAGS -c mlir_parser.c -o mlir_parser.o
-clang $CFLAGS -c parser.cpp -o parser.o
+clang $CFLAGS -c parser.c -o parser.o
 clang $CFLAGS -o parser parser.o tokenizer.o arena.o mlir_parser.o
 ./parser
