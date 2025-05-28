de59af9da2a0a90b846c22b1809d31eb0e878599
Ondřej Čertík <ondrej@certik.us>
2025-05-28 09:00:44 -0600

    Works

--- msvc_build.sh
+++ msvc_build.sh
@@ -1,4 +1,4 @@
 set -ex
 
-cl /Zc:preprocessor /I. /Fe:test_format.exe tests/test_format.c base/arena.c base/string.c base/format.c
+cl /std:c11 /Zc:preprocessor /I. /Fe:test_format.exe tests/test_format.c base/arena.c base/string.c base/format.c
 ./test_format
