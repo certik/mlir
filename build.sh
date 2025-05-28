903f5433b29ad24545b6a7b3fc9dce3d86aeda6f
Ondřej Čertík <ondrej@certik.us>
2025-05-28 18:02:05 +0200

    Rename

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -ex
 
 CFLAGS="-fsanitize=address -g -Wall -ferror-limit=1"
 
-clang $CFLAGS -I. -o test tests/tests.c base/arena.c base/string.c base/format.c base/io.c
-./test
+clang $CFLAGS -I. -o run_tests tests/run_tests.c base/arena.c base/string.c base/format.c base/io.c
+./run_tests
 
 #re2c -b tokenizer.re -o tokenizer.c
 #clang $CFLAGS -I. -o parser parser.c tokenizer.c base/arena.c base/io.c mlir_parser.c
