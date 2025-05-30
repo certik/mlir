7bc696e4b26ee1af64796600d505d42fdb3d73df
Ondřej Čertík <ondrej@certik.us>
2025-05-30 15:57:46 -0600

    Update MSVC build

--- msvc_build.sh
+++ msvc_build.sh
@@ -3,5 +3,5 @@ set -ex
 cl /std:c11 /Zc:preprocessor /I. /Fe:run_tests.exe tests/run_tests.c base/arena.c base/string.c base/format.c base/io.c
 ./run_tests
 
-cl /std:c11 /Zc:preprocessor /I. /Fe:parser.exe parser.c tokenizer.c mlir_parser.c base/arena.c base/string.c base/format.c base/io.c
+cl /std:c11 /Zc:preprocessor /I. /Fe:parser.exe parser.c tokenizer.c mlir_parser.c base/arena.c base/string.c base/format.c base/io.c base/vector.c
 ./parser
