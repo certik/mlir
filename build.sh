7703694690ff20c8350af5c1ddd0ccdf9c1fe9a9
Ondřej Čertík <ondrej@certik.us>
2025-05-31 22:40:54 +0200

    Run everything

--- build.sh
+++ build.sh
@@ -10,4 +10,6 @@ clang $CFLAGS -I. -o run_tests tests/run_tests.c base/arena.c base/string.c base
 re2c --no-generation-date -b tokenizer.re -o tokenizer.c
 clang $CFLAGS -I. -o parser parser.c tokenizer.c mlir_parser.c base/arena.c base/string.c base/format.c base/io.c base/vector.c
 ./parser
+./parser a.mlir
 ./parser b.mlir
+./parser c.mlir
