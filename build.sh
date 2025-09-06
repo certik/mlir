a68705c44edb721b9f5dd1b8ac035c4664bf1bb6
Ondřej Čertík <ondrej@certik.us>
2025-09-05 21:03:37 -0600

    Don't run C tests

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -ex
 CFLAGS="-fsanitize=address -g -Wall -ferror-limit=1"
 
 clang $CFLAGS -I. -o run_tests tests/run_tests.c base/arena.c base/string.c base/format.c base/io.c
-./run_tests
+#./run_tests
 
 re2c --no-generation-date -b tokenizer.re -o tokenizer.c
 clang $CFLAGS -I. -o parser parser.c tokenizer.c mlir_parser.c mlir_generic_printer.c mlir_classic_printer.c op_parsers.c base/arena.c base/string.c base/format.c base/io.c
