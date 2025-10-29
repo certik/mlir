e2189d5da6644e8444e7f21dbe7899939fd062c1
Ondřej Čertík <ondrej@certik.us>
2025-10-29 09:35:16 -0700

    Do not run re2c

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ CFLAGS="-fsanitize=address -g -Wall -ferror-limit=1"
 clang $CFLAGS -I. -o run_tests tests/run_tests.c base/arena.c base/string.c base/format.c base/io.c
 #./run_tests
 
-re2c --no-generation-date -b tokenizer.re -o tokenizer.c
+#re2c --no-generation-date -b tokenizer.re -o tokenizer.c
 clang $CFLAGS -I. -o parser parser.c tokenizer.c mlir_parser.c mlir_generic_printer.c mlir_classic_printer.c op_parsers.c mlir_api_impl.c base/arena.c base/string.c base/format.c base/io.c
 ./parser
 ./parser --construct
